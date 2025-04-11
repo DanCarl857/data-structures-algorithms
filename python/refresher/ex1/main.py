d3961c773c0ab136e82f12136aa049b64f6117e4
Daniel Carlson <dancarl758@gmail.com>
2025-04-11 15:07:09 +0100

    feat(refresher): basic completed

---
+++ python/refresher/ex1/main.py
@@ -0,0 +1,30 @@
+print("Hello World")
+print("This is fun.")
+
+cars = 100
+drivers = 30
+
+print("There are", cars, "cars available")
+print("There are only", drivers, "drivers available")
+
+types_of_people = 10
+x = f"There are {types_of_people} types of people"
+
+print(x)
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print(w + e)
+
+
+print("How old are you?", end=" ")
+age = input()
+
+print("How tall are you?", end=" ")
+height = input()
+
+print(f"So you're {age} old, {height} tall.")
+
+age1 = input("How old are you? ")
+
---
+++ python/refresher/ex1/unpack.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print("The script is called: ", script)
+print("Your first argument is: ", first)
+print("Your second argument is: ", second)
+print("Your third argument is: ", third)
